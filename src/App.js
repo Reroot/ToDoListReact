ae05678d4ee6d514b37fc883b24dd773be54ca92
Artem <guts@Artems-MacBook-Air.local>
2019-11-22 00:14:21 -0800

    conversion to classes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import toDoData from "./toDoData";
 import TodoItem from "./TodoItem";
-function App() {
-	const toDoItemsArray = toDoData.map((item) => (
-		<TodoItem key={item.id} item={item} />
-	));
 
-	console.log(toDoItemsArray);
-
-	return <div className="App">{toDoItemsArray}</div>;
+export class App extends Component {
+	render() {
+		return <div>{toDoItemsArray}</div>;
+	}
 }
+
+const toDoItemsArray = toDoData.map((item) => (
+	<TodoItem key={item.id} item={item} />
+));
+
+// function createList() {
+// 	const toDoItemsArray = toDoData.map((item) => (
+// 		<TodoItem key={item.id} item={item} />
+// 	));
+
+// 	console.log(toDoItemsArray);
+
+// 	return { toDoItemsArray };
+// }
 
 export default App;
